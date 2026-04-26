495236487a161a3fd8f346045da46b77d4c13e60
Rashid <rashidmubasshir@gmail.com>
2026-04-26 12:48:49 +0530

    variables and strings

---
+++ string_variables.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const brand = "Apple";
+let price = 59.99;
+let discount = 12.99;
+let final_price = price - discount;
+
+console.log(`The brand name of the phone is ${brand}`);
+console.log(`The initial price of the product is ${price}`);
+console.log(`The discount on this product is ${discount}`);
+console.log(`So, your final price for an Apple phone would be ${final_price}`);
